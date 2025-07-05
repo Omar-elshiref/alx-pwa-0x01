37786eb0ec53da10f800f4b3d72c064c02ce419d
Omar mohamed <omarabdou2002@yahoo.com>
2025-07-05 21:47:12 +0300

    Done 5

--- alx-movie-app/pages/index.tsx
+++ alx-movie-app/pages/index.tsx
@@ -14,12 +14,12 @@ const Home: React.FC = () => {
       >
         <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center text-center">
           <h1 className="text-5xl md:text-7xl font-bold mb-8">
-            Discover Your Next Favorite{" "}
+            Discover Your Next Favorite
             <span className="text-[#E2D609]">Movie</span>
           </h1>
           <p className="text-lg md:text-2xl mb-8 max-w-2xl">
             Explore the latest blockbuster movies, critically acclaimed films,
-            and your personal favorites – all in one place.
+            and your personal favorites all in one place.
           </p>
           <Button
             title="Browse Movies"
